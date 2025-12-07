a4ed44391675315cf707dc788d9fa7bc4306bbd0
Humayra Binte Jamal <humayra1111@gmail.com>
2025-12-07 15:29:39 +0600

    first commit

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => res.json({ status: 'ok' }));
+export default app;
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ status: 'ok' }));
+
+export default app;
